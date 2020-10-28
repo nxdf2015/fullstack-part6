646ee82e3f422a0ddc5e7b0a5e0ce2eeddacceca
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-10-28 14:00:06 +0100

    part6 13

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteFilter from './components/AnecdoteFilter'
 import Notification from './components/Notification'
 
+import { useDispatch } from 'react-redux'
+import { anecdoteActions } from './reducers'
+import { getAll } from './services/anecdotes'
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    getAll().then((response) =>
+      dispatch(anecdoteActions.initAnecdote(response))
+    )
+  }, [dispatch])
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification/>
-      <AnecdoteFilter/>
+      <Notification />
+      <AnecdoteFilter />
       <AnecdoteForm />
       <AnecdoteList />
     </div>
